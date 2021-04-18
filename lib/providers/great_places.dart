a91a0b625dab38b5aa5fb9a2d29eea07bbae43c7
mohak <mohaknigam20@gmail.com>
2021-04-18 18:58:54 +0530

    location setup

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:places/helpers/db_helper.dart';
 import 'package:places/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -20,6 +21,27 @@ class GreatPlaces with ChangeNotifier {
       location: null,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+      },
+    );
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((item) => Place(
+              id: item['id'],
+              title: item['title'],
+              image: File(item['image']),
+              location: null,
+            ))
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -18,28 +18,37 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Text('Got No Places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemBuilder: (ctx, i) => Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: FileImage(
-                          greatPlaces.items[i].image,
-                        ),
-                      ),
-                      title: Text(greatPlaces.items[i].title),
-                      onTap: () {},
-                    ),
-                    Divider(),
-                  ],
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Text('Got No Places yet, start adding some!'),
                 ),
-                itemCount: greatPlaces.items.length,
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemBuilder: (ctx, i) => Column(
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: FileImage(
+                                  greatPlaces.items[i].image,
+                                ),
+                              ),
+                              title: Text(greatPlaces.items[i].title),
+                              onTap: () {},
+                            ),
+                            Divider(),
+                          ],
+                        ),
+                        itemCount: greatPlaces.items.length,
+                      ),
               ),
       ),
     );
